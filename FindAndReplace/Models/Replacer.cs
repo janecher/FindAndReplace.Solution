e31957e84ca561ef5cb1ace8a9cc29edfbf28e18
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-16 22:44:28 -0700

    add functionality to prepare text for/and replacement

--- FindAndReplace/Models/Replacer.cs
+++ FindAndReplace/Models/Replacer.cs
@@ -1,7 +1,30 @@
+using System;
+
 namespace FindAndReplace
 {
   public class Replacer
   {
-    // properties, constructors, methods, etc. go here
+    public static string FindAndReplace(string text, string word, string replacer)
+    {
+      string modifyText = RemoveBlankSpaces(text);
+      string[] splitText = modifyText.Split(' ');
+      for(int i = 0; i < splitText.Length; i++)
+      {
+        if(splitText[i].Contains(word))
+        {
+          splitText[i] = splitText[i].Replace(word, replacer);
+        }
+      }
+      return String.Join(' ', splitText);
+    }
+    public static string RemoveBlankSpaces(string text)
+    {
+      string trimText = text.Trim();
+      while(trimText.Contains("  "))
+      {
+        trimText.Replace("  ", " ");
+      }
+      return trimText;
+    }
   }
 }
