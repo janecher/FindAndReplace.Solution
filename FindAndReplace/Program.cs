f4fb4a96f734fada6d9154770e9ab1c87fa98ccc
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-16 22:49:59 -0700

    add UI logic

--- FindAndReplace/Program.cs
+++ FindAndReplace/Program.cs
@@ -1,10 +1,15 @@
 using System;
-using System.Collections.Generic; //remove if not using Dictionary in app //
-
+using FindAndReplace;
 class Program
 {
   static void Main()
   {
-    
+    Console.WriteLine("Input the text:");
+    string text = Console.ReadLine();
+    Console.WriteLine("Input the word or part the word needs to be replaced");
+    string word = Console.ReadLine();
+    Console.WriteLine("Input the replacer word");
+    string replacer = Console.ReadLine();
+    Console.WriteLine(Replacer.FindAndReplace(text, word, replacer));
   }
 }
